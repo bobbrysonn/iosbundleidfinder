d72e16f745fd4c076b82f4a9621010abf865eed3
Bob Moriasi <official.bobmoriasi@gmail.com>
2024-09-09 09:56:21 +0300

    Footer component

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="text-center py-4">
+      <p className="mt-4 text-sm text-muted-foreground">
+        Made with ❤️ by <a href="https://bobbrysonn.dev">Bob Moriasi</a>
+      </p>
+    </footer>
+  );
+}
