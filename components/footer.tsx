f24e11edc38dfeb44e719256e4026f6b98fbf2aa
Bob Moriasi <official.bobmoriasi@gmail.com>
2024-09-09 10:05:40 +0300

    Added hover to footer link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,15 @@
 export default function Footer() {
   return (
-    <footer className="text-center py-4">
-      <p className="mt-4 text-sm text-muted-foreground">
-        Made with ❤️ by <a href="https://bobbrysonn.dev">Bob Moriasi</a>
+    <footer className="text-center py-4 mt-4">
+      <p className="text-sm text-muted-foreground inline">
+        Made with ❤️ by {""}
       </p>
+      <a
+        href="https://bobbrysonn.dev"
+        className="transition-colors text-muted-foreground text-sm hover:text-foreground duration-300"
+      >
+        Bob Moriasi
+      </a>
     </footer>
   );
 }
